dc2988c76d1518202df9388ad4bcf4964886cc8c
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-02-26 21:30:42 +0600

    0169-MAJORITY-ELEMENT Stats: Time: 0 ms (100%), Space: 28.2 MB (62.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        return nums[n/2];
+
+        //approch 1 : nlogn
+        // int n = nums.size();
+        // sort(nums.begin(), nums.end());
+        // return nums[n/2];
+
+        //approch 2 : o(n)
+        int counter = 0;
+        int candidate = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(counter == 0){
+                candidate = nums[i];
+                counter = 1;
+            }
+            else if(nums[i] == candidate) counter++;
+            else counter--;
+        }
+        return candidate;
     }
 };
