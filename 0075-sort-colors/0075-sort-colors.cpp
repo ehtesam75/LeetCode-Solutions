25512222f73a0c758025e8294e5cf5b4498ded16
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-03-01 22:31:06 +0600

    0075-SORT-COLORS Stats: Time: 0 ms (100%), Space: 11.5 MB (77.95%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int r, w, b;
-        r = w = b = 0;
+        //approch 1:
+        // int r, w, b;
+        // r = w = b = 0;
 
-        for(int x : nums){
-            if(x == 0) r++;
-            else if(x == 1) w++;
-            else b++;
-        }
+        // for(int x : nums){
+        //     if(x == 0) r++;
+        //     else if(x == 1) w++;
+        //     else b++;
+        // }
 
-        int i = 0;
-        while(r--){
-            nums[i++] = 0;
-        }
+        // int i = 0;
+        // while(r--){
+        //     nums[i++] = 0;
+        // }
 
-        while(w--){
-            nums[i++] = 1;
-        }
+        // while(w--){
+        //     nums[i++] = 1;
+        // }
 
-        while(b--){
-            nums[i++] = 2;
+        // while(b--){
+        //     nums[i++] = 2;
+        // }
+
+
+        //approch 2: using three pointers
+        int n = nums.size();
+        int l = 0, r = n - 1, i = 0;
+
+        while(i <= r){
+            if(nums[i] == 0){
+                swap(nums[i++], nums[l++]);
+            }
+
+            else if (nums[i] == 2){
+                swap(nums[i], nums[r--]);
+            }
+            else{
+                i++;
+            }
         }
     }
 };
