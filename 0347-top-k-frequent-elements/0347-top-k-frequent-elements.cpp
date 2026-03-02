7f0b99478ff63033d010f77e07c5175e4ccc7678
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-03-02 17:19:12 +0600

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 4 ms (35.18%), Space: 19.8 MB (17.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,4 +1,4 @@
-static const int N = 2e4 + 7;
+// static const int N = 2e4 + 7;
 
 class Solution {
 
@@ -27,25 +27,50 @@ public:
         //     }
         //     else ans.push_back(-(v[i].second - 1e4));
         // }
+        // return ans;
 
 
         // approch 2 : using map
+        // unordered_map<int, int> mp;
+        // for(auto x : nums){
+        //     mp[x]++;
+        // }
+
+        // vector<pair<int, int>> ans;
+        // for(auto [u, v] : mp){
+        //     ans.push_back({v, u});
+        // }
+
+        // sort(ans.begin(), ans.end(), greater<pair<int, int>>());
+
+        // vector<int> sol;
+        // for(int i = 0; i < k; i++){
+        //     sol.push_back(ans[i].second);
+        // }
+        // return sol;
+
+
+        //approch 3 : using bucket sort (o(n) time)
+        int n = nums.size();
         unordered_map<int, int> mp;
+        vector<vector<int>> v (n + 1);
         for(auto x : nums){
             mp[x]++;
         }
 
-        vector<pair<int, int>> ans;
-        for(auto [u, v] : mp){
-            ans.push_back({v, u});
+        for(auto p : mp){
+            int freq = p.second;
+            v[freq].push_back(p.first);
         }
 
-        sort(ans.begin(), ans.end(), greater<pair<int, int>>());
-
-        vector<int> sol;
-        for(int i = 0; i < k; i++){
-            sol.push_back(ans[i].second);
+        vector<int> ans;
+        for(int i = n; i >= 0; i--){
+            for(int j = 0; j < v[i].size(); j++){
+                ans.push_back(v[i][j]);
+                k--;
+                if(k == 0) return ans;
+            }
         }
-        return sol;
+        return ans;
     }
 };
