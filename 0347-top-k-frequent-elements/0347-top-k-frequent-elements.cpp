2b1697ebad8744803d091900a6c79634dbfde510
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-03-02 16:42:49 +0600

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 7 ms (16.37%), Space: 18 MB (39.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,28 +4,48 @@ class Solution {
 
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int> freq(N, 0);
-        vector<pair<int, int>> v;
-        
-        for(int x : nums){
-            if(x >= 0) freq[x]++;
-            else freq[1e4 + abs(x)]++;
+        //approch 1 : :using vector (nlogn)
+        // vector<int> freq(N, 0);
+        // vector<pair<int, int>> v;
+
+        // for(int x : nums){
+        //     if(x >= 0) freq[x]++;
+        //     else freq[1e4 + abs(x)]++;
+        // }
+
+        // for(int i = 0; i < N; i++){
+        //     if(freq[i] > 0){
+        //         v.push_back({freq[i], i});
+        //     }
+        // }
+
+        // vector<int> ans;
+        // sort(v.begin(), v.end(), greater<pair<int, int>>());
+        // for(int i = 0; i < k; i++){
+        //     if(v[i].second <= 1e4){
+        //         ans.push_back(v[i].second);
+        //     }
+        //     else ans.push_back(-(v[i].second - 1e4));
+        // }
+
+
+        // approch 2 : using map
+        unordered_map<int, int> mp;
+        for(auto x : nums){
+            mp[x]++;
         }
 
-        for(int i = 0; i < N; i++){
-            if(freq[i] > 0){
-                v.push_back({freq[i], i});
-            }
+        vector<pair<int, int>> ans;
+        for(auto [u, v] : mp){
+            ans.push_back({v, u});
         }
 
-        vector<int> ans;
-        sort(v.begin(), v.end(), greater<pair<int, int>>());
+        sort(ans.begin(), ans.end(), greater<pair<int, int>>());
+
+        vector<int> sol;
         for(int i = 0; i < k; i++){
-            if(v[i].second <= 1e4){
-                ans.push_back(v[i].second);
-            }
-            else ans.push_back(-(v[i].second - 1e4));
+            sol.push_back(ans[i].second);
         }
-        return ans;
+        return sol;
     }
 };
