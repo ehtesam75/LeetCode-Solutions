2b736f35125abb93f0a4a68a95538c31d6bf67b8
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-03-02 23:49:00 +0600

    0238-PRODUCT-OF-ARRAY-EXCEPT-SELF Stats: Time: 0 ms (100%), Space: 40.3 MB (58.74%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        //approach 1 : time O(n) and space o(n)
+        // int n = nums.size();
+        // vector<int> prefix(n, 1);
+        // vector<int> suffix(n, 1);
+
+        // prefix[0] = nums[0];
+        // for(int i = 1; i < n; i++){
+        //     prefix[i] = prefix[i-1] * nums[i];
+        // }
+
+        // suffix[n-1] = nums[n-1];
+        // for(int i = n - 2; i >= 0; i--){
+        //     suffix[i] = suffix[i+1] * nums[i];
+        // }
+
+        // vector<int> ans(n);
+        // int total;
+        // for(int i = 0; i < n; i++){
+        //     total = 1;
+        //     if(i > 0) total *= prefix[i-1];
+        //     if(i < n-1) total *= suffix[i+1];
+        //     ans[i] = total;
+        // }
+        // return ans;
+
+        //approach 2:time o(n) and space o(n) (O(1) extra space complexity)
         int n = nums.size();
-        vector<int> prefix(n, 1);
-        vector<int> suffix(n, 1);
+        vector<int> res(n, 1);
 
-        prefix[0] = nums[0];
+        res[0] = 1;
         for(int i = 1; i < n; i++){
-            prefix[i] = prefix[i-1] * nums[i];
+            res[i] = res[i-1] * nums[i-1];
         }
 
-        suffix[n-1] = nums[n-1];
-        for(int i = n - 2; i >= 0; i--){
-            suffix[i] = suffix[i+1] * nums[i];
+        int suffix = 1;
+        for(int i = n-1; i >= 0; i--){
+            res[i] *= suffix;
+            suffix *= nums[i];
         }
-
-        vector<int> ans(n);
-        int total;
-        for(int i = 0; i < n; i++){
-            total = 1;
-            if(i > 0) total *= prefix[i-1];
-            if(i < n-1) total *= suffix[i+1];
-            ans[i] = total;
-        }
-        return ans;
+        return res;
     }
 };
