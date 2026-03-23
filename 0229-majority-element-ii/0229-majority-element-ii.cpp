f7abe558bfeafea7f3d2bef6dd91c5dfac675f8a
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-03-23 17:41:37 +0600

    0229-MAJORITY-ELEMENT-II Stats: Time: 7 ms (41.95%), Space: 26.4 MB (27.5%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
         vector<int> ans;
 
-        int cnt_cosec = 1;
-        for(int i = 1; i < n; i++){
-            if(nums[i] == nums[i-1]){
-                cnt_cosec++;
-            }
-            else{
-                if(cnt_cosec > (n/3)){
-                    ans.push_back(nums[i-1]);
-                }
-                cnt_cosec = 1;
-            }
+        //approch 1 : o(nlogn) time and o(1) space
+        // sort(nums.begin(), nums.end());
+
+        // int cnt_cosec = 1;
+        // for(int i = 1; i < n; i++){
+        //     if(nums[i] == nums[i-1]){
+        //         cnt_cosec++;
+        //     }
+        //     else{
+        //         if(cnt_cosec > (n/3)){
+        //             ans.push_back(nums[i-1]);
+        //         }
+        //         cnt_cosec = 1;
+        //     }
+        // }
+
+        // if(cnt_cosec > (n/3)){
+        //     ans.push_back(nums[n-1]);
+        // }
+        // return ans;
+
+
+        //approch 2 : o(logn) time & o(n) space
+        unordered_map<int, int> mp;
+        for(auto x : nums){
+            mp[x]++;
         }
 
-        if(cnt_cosec > (n/3)){
-            ans.push_back(nums[n-1]);
+        for(auto [u, v] : mp){
+            if(v > (n/3)){
+                ans.push_back(u);
+            }
         }
         return ans;
     }
