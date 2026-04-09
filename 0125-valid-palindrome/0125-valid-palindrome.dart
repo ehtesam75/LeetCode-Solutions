af436c355e31402c2beb233fdca3890f20db74a7
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-04-09 18:07:38 +0600

    0125-VALID-PALINDROME Stats: Time: 18 ms (42.97%), Space: 155.1 MB (8.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.dart
+++ 0125-valid-palindrome/0125-valid-palindrome.dart
@@ -1,18 +1,34 @@
 class Solution {
   bool isPalindrome(String s) {
-    String temp = "";
-    for(var x in s.split('')){
-        if(RegExp(r'^[a-zA-Z0-9]+$').hasMatch(x)){
-            temp += x.toLowerCase();
-        }
-    }
-    print(temp);
+    //approch 1:
+    // String temp = "";
+    // for(var x in s.split('')){
+    //     if(RegExp(r'^[a-zA-Z0-9]+$').hasMatch(x)){
+    //         temp += x.toLowerCase();
+    //     }
+    // }
 
-    int n = temp.length;
-    for(int i = 0; i < n/2; i++){
-        if(temp[i] != temp[n-i-1]){
+    // print(temp);
+
+    // int n = temp.length;
+    // for(int i = 0; i < n/2; i++){
+    //     if(temp[i] != temp[n-i-1]){
+    //         return false;
+    //     }
+    // }
+    // return true;
+
+    //approch 2:
+    String cleaned = s.toLowerCase().replaceAll(RegExp(r'[^a-z0-9]'), '');
+    print(cleaned);
+    int i = 0;
+    int j = cleaned.length-1;
+
+    while(i < j){
+        if(cleaned[i] != cleaned[j]){
             return false;
         }
+        i++; j--;
     }
     return true;
   }
