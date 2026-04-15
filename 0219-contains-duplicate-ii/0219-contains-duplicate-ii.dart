858e121a6b7d998257383367e7815f6cdefc511a
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-04-15 16:27:05 +0600

    0219-CONTAINS-DUPLICATE-II Stats: Time: 35 ms (80.95%), Space: 183 MB (72.62%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.dart
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.dart
@@ -1,14 +1,31 @@
 class Solution {
   bool containsNearbyDuplicate(List<int> nums, int k) {
     int n = nums.length;
-    Map<int, int> mp = {};
+    //approch 1 : o(n) time and o(n) space
+    // Map<int, int> mp = {};
 
+    // for(int i = 0; i < n; i++){
+    //     if(mp.containsKey(nums[i])){
+    //         int lastPos = mp[nums[i]]!;
+    //         if(i-lastPos <= k) return true;
+    //     }
+    //     mp[nums[i]] = i;
+    // }
+    // return false;
+
+
+    //approch 2 :sliding window o(n) time o(1) space
+
+    final window = <int>{};
     for(int i = 0; i < n; i++){
-        if(mp.containsKey(nums[i])){
-            int lastPos = mp[nums[i]]!;
-            if(i-lastPos <= k) return true;
+        if(window.contains(nums[i])){
+            return true;
         }
-        mp[nums[i]] = i;
+        window.add(nums[i]);
+
+        if(window.length > k){
+            window.remove(nums[i-k]);
+        }
     }
     return false;
   }
