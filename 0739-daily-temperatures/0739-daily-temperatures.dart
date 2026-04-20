62e92798187c9c08dcf46d26f299fc0b0153383b
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-04-20 19:16:49 +0600

    0739-DAILY-TEMPERATURES Stats: Time: 19 ms (61.11%), Space: 192.8 MB (5.56%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.dart
+++ 0739-daily-temperatures/0739-daily-temperatures.dart
@@ -1,23 +1,37 @@
 class Solution {
   List<int> dailyTemperatures(List<int> temperatures) {
-    List<(int, int)> st = [];
-    Map<int, int> mp = {};
-    for(int i = 0; i < temperatures.length; i++){
-        while(st.isNotEmpty && st.last.$1 < temperatures[i]){
-            int removed = st.last.$2;
-            st.removeLast();
-            mp[removed] = i;
-        }
-        st.add((temperatures[i], i));
-    }
+    //approch 1 : o(n) time
+    // List<(int, int)> st = [];
+    // Map<int, int> mp = {};
+    // for(int i = 0; i < temperatures.length; i++){
+    //     while(st.isNotEmpty && st.last.$1 < temperatures[i]){
+    //         int removed = st.last.$2;
+    //         st.removeLast();
+    //         mp[removed] = i;
+    //     }
+    //     st.add((temperatures[i], i));
+    // }
 
-    List<int> ans = [];
+    // List<int> ans = [];
+    // for(int i = 0; i < temperatures.length; i++){
+    //     if(mp.containsKey(i)){
+    //         ans.add(mp[i]! - i);
+    //     }
+    //     else ans.add(0);
+    // }
+    // return ans;
+
+
+    //approch 2 : o(n) time but more optimized
+    List<int> st = [];
+    List<int> ans = List.filled(temperatures.length, 0);
+
     for(int i = 0; i < temperatures.length; i++){
-        if(mp.containsKey(i)){
-            ans.add(mp[i]! - i);
-            // print(mp[temperatures[i]]!);
+        while(st.isNotEmpty && temperatures[st.last] < temperatures[i]){
+            int removed = st.removeLast();
+            ans[removed] = i - removed;
         }
-        else ans.add(0);
+        st.add(i);
     }
     return ans;
   }
