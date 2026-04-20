f21f22cc2b47c5cb0e4583393cd44790b4f0536e
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-04-20 18:32:22 +0600

    0496-NEXT-GREATER-ELEMENT-I Stats: Time: 5 ms (91.67%), Space: 150.1 MB (59.72%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.dart
+++ 0496-next-greater-element-i/0496-next-greater-element-i.dart
@@ -11,10 +11,31 @@ int find_grater(List<int> nums, int curr){
 
 class Solution {
   List<int> nextGreaterElement(List<int> nums1, List<int> nums2) {
-    //approch 1:
+    //approch 1: time : o(n * m)
+    // List<int> ans = [];
+    // for(int i = 0; i < nums1.length; i++){
+    //     ans.add(find_grater(nums2, nums1[i]));
+    // }
+    // return ans;
+
+    //approch 2 : time o(n) using monotonic stack
+    List<int> st = [];
+    Map<int, int> mp = {};
+    for(int i = 0; i < nums2.length; i++){
+        while(st.isNotEmpty && st.last < nums2[i]){
+            int removed = st.last;
+            st.removeLast();
+            mp[removed] = nums2[i];
+        }
+        st.add(nums2[i]);
+    }
+
     List<int> ans = [];
     for(int i = 0; i < nums1.length; i++){
-        ans.add(find_grater(nums2, nums1[i]));
+        if(mp.containsKey(nums1[i])){
+            ans.add(mp[nums1[i]]!);
+        }
+        else ans.add(-1);
     }
     return ans;
   }
