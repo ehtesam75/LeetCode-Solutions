d4f5ec607eace3fb00836f426a1e9f507f342c99
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-04-22 00:30:56 +0600

    0448-FIND-ALL-NUMBERS-DISAPPEARED-IN-AN-ARRAY Stats: Time: 6 ms (79.41%), Space: 171.8 MB (79.41%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.dart
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.dart
@@ -1,16 +1,26 @@
 class Solution {
   List<int> findDisappearedNumbers(List<int> nums) {
     int n = nums.length;
+    List<int> ans = [];
 
     //approch 1 : 0(n) time and o(1) space
-    List<bool> arr = List.filled(n+1, false);
+    // List<bool> arr = List.filled(n+1, false);
+    // for(int i = 0; i < n; i++){
+    //     arr[nums[i]] = true;
+    // }
+
+    // for(int i = 1; i <= n; i++){
+    //     if(!arr[i]) ans.add(i);
+    // }
+
+    //approch 2: better space
     for(int i = 0; i < n; i++){
-        arr[nums[i]] = true;
+        int val = nums[i].abs();
+        if(nums[val-1] > 0) nums[val-1] = -nums[val-1];
     }
 
-    List<int> ans = [];
-    for(int i = 1; i <= n; i++){
-        if(!arr[i]) ans.add(i);
+    for(int i = 0; i < n; i++){
+        if(nums[i] > 0) ans.add(i+1);
     }
     return ans;
   }
