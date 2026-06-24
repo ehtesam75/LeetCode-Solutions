c6dcf2f19cf4b75da6acc9d369db78ca789dce47
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-06-24 21:24:10 +0600

    0002-ADD-TWO-NUMBERS Stats: Time: 0 ms (100%), Space: 19.4 MB (40.99%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,70 +5,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # #list1 reverse
-        # prev1 = None
-        # curr1 = l1
-
-        # while curr1:
-        #     nxt = curr1.next
-        #     curr1.next = prev1
-
-        #     prev1 = curr1
-        #     curr1 = nxt
-
-        # #list2 reverse
-        # prev2 = None
-        # curr2 = l2
-
-        # while curr2:
-        #     nxt = curr2.next
-        #     curr2.next = prev2
-
-        #     prev2 = curr2
-        #     curr2 = nxt
-
-        
-        temp1 = l1
-        temp2 = l2
         dummy = ListNode(-1)
         mov = dummy
 
         carry = 0
-        while temp1 and temp2:
-            sum = temp1.val + temp2.val + carry
-            mov.next = ListNode(sum % 10)
-            if temp1.val + temp2.val + carry > 9:
-                carry = 1
-            else:
-                carry = 0
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
 
-            temp1 = temp1.next
-            temp2 = temp2.next
-            mov = mov.next
-        
-        while temp1:
-            sum = temp1.val + carry
-            mov.next = ListNode(sum % 10)
-            if temp1.val + carry > 9:
-                carry = 1
-            else:
-                carry = 0
+            total = val1 + val2 + carry
+            carry = total // 10
 
-            temp1 = temp1.next
+            mov.next = ListNode(total % 10)
             mov = mov.next
 
-        while temp2:
-            sum = temp2.val + carry
-            mov.next = ListNode(sum % 10)
-            if temp2.val + carry > 9:
-                carry = 1
-            else:
-                carry = 0
-
-            temp2 = temp2.next
-            mov = mov.next
-        
-        if carry:
-            mov.next = ListNode(carry)
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
 
         return dummy.next
